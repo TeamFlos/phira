80e16c174cb1e0220767eaab14beb5fcf41a6288
mivik <mivik@qq.com>
2023-04-12 23:19:50 +0800

    fix: iOS 13

--- prpr/src/scene.rs
+++ prpr/src/scene.rs
@@ -251,7 +251,7 @@ pub fn request_file(id: impl Into<String>) {
                     msg_send![picker, initForOpeningContentTypes: types]
                 } else {
                     let ext = |e: &str| str_to_ns(e);
-                    let types = NSArray::from_vec(vec![ext("zip"), ext("pez"), ext("jpg"), ext("png"), ext("jpeg")]);
+                    let types = NSArray::from_vec(vec![ext(".zip"), ext(".pez"), ext(".jpg"), ext(".png"), ext(".jpeg")]);
                     let types: ObjcId = std::mem::transmute(types);
                     msg_send![picker, documentTypes: types inMode: 0]
                 };
