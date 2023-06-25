0d5e0808e865dfd70bbdc20580446a3d1f23957b
mivik <mivik@qq.com>
2023-06-25 16:08:00 +0800

    feat: add monitor (incomplete)

---
+++ phira-monitor/src/main.rs
@@ -0,0 +1,92 @@
+mod scene;
+
+use std::fs::File;
+
+use anyhow::{Context, Result};
+use macroquad::prelude::*;
+use phira_mp_common::RoomId;
+use prpr::{
+    core::init_assets,
+    scene::show_error,
+    time::TimeManager,
+    ui::{FontArc, TextPainter, Ui},
+    Main,
+};
+use scene::MainScene;
+use serde::Deserialize;
+
+#[derive(Clone, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Config {
+    server: String,
+
+    email: String,
+    password: String,
+
+    room_id: String,
+}
+
+pub fn build_conf() -> macroquad::window::Conf {
+    macroquad::window::Conf {
+        window_title: "prpr-monitor".to_string(),
+        window_width: 1080,
+        window_height: 608,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(build_conf)]
+async fn main() {
+    if let Err(err) = the_main().await {
+        eprintln!("{err:?}");
+    }
+}
+
+async fn the_main() -> Result<()> {
+    pretty_env_logger::init();
+
+    init_assets();
+
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .worker_threads(4)
+        .enable_all()
+        .build()
+        .unwrap();
+    let _guard = rt.enter();
+
+    let font = FontArc::try_from_vec(load_file("font.ttf").await?)?;
+    let mut painter = TextPainter::new(font);
+
+    let config: Config = (|| -> Result<Config> { Ok(serde_yaml::from_reader(File::open("monitor-config.yml")?)?) })().context("读取配置失败")?;
+
+    let mut main = Main::new(Box::new(MainScene::new(config).await?), TimeManager::default(), None).await?;
+
+    let tm = TimeManager::default();
+    let mut fps_time = -1;
+    'app: loop {
+        let frame_start = tm.real_time();
+        let res = || -> Result<()> {
+            main.update()?;
+            main.render(&mut Ui::new(&mut painter))?;
+            Ok(())
+        }();
+        if let Err(err) = res {
+            warn!("uncaught error: {:?}", err);
+            show_error(err);
+        }
+        if main.should_exit() {
+            break 'app;
+        }
+
+        let t = tm.real_time();
+        let fps_now = t as i32;
+        if fps_now != fps_time {
+            fps_time = fps_now;
+            info!("| {}", (1. / (t - frame_start)) as u32);
+        }
+
+        next_frame().await;
+    }
+
+    Ok(())
+}
