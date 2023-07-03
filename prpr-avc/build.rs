18ef6eae1fbc01cbbe4ec7706a3d8a92be27c02d
sjfhsjfh <sjfhsjfh@qq.com>
2023-07-03 13:38:28 +0800

    fix: iOS build

--- prpr-avc/build.rs
+++ prpr-avc/build.rs
@@ -1,4 +1,6 @@
 fn main() {
-    println!("cargo:rustc-link-search={}/static-lib/{}", std::env::var("CARGO_MANIFEST_DIR").unwrap(), std::env::var("TARGET").unwrap());
+    let lib_path = format!("{}/static-lib/{}", std::env::var("CARGO_MANIFEST_DIR").unwrap(), std::env::var("TARGET").unwrap());
+    println!("cargo:rustc-link-search={lib_path}");
     println!("cargo:rustc-link-lib=z");
+    println!("cargo:rerun-if-changed={lib_path}");
 }
