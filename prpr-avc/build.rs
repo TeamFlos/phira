aa5c473ce9b7b8b6710c8d6418ef8517e2dd2d47
Dage-Hoo <dagehoo@outlook.com>
2023-11-19 13:16:57 +0800

    Remove useless code

--- prpr-avc/build.rs
+++ prpr-avc/build.rs
@@ -4,7 +4,6 @@ fn main() {
     let libs_dir = std::env::var("PRPR_AVC_LIBS").unwrap_or_else(|_| format!("{}/static-lib", std::env::var("CARGO_MANIFEST_DIR").unwrap()));
     let libs_path = Path::new(&libs_dir).join(std::env::var("TARGET").unwrap());
     let libs_path = libs_path.display();
-    println!("{libs_path}");
     println!("cargo:rustc-link-search={libs_path}");
     println!("cargo:rustc-link-lib=z");
     println!("cargo:rustc-link-lib=lzma");
