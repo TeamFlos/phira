a27c88a8c0829539348babcb69d5961eda89205d
sjfhsjfh <58244206+sjfhsjfh@users.noreply.github.com>
2024-02-08 23:44:15 +0800

    fix: repack info dialog

--- phira/src/page/respack.rs
+++ phira/src/page/respack.rs
@@ -149,7 +149,7 @@ impl Page for ResPackPage {
                 tl!("info"),
                 tl!("info-content", "name" => item.name.clone(), "author" => info.author.clone(), "desc" => info.description.clone()),
             )
-            .listener(|index| index == 2)
+            .listener(|index| index == -2)
             .show();
             return Ok(true);
         }
