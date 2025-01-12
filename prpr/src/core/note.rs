a7261f367f685ee3a9ab484f7c64956cb852ee21
HLMC <hl2278535805@outlook.com>
2025-01-12 14:38:39 +0800

    fix: cover pos

--- prpr/src/core/note.rs
+++ prpr/src/core/note.rs
@@ -213,7 +213,7 @@ impl Note {
 
         let base = height - line_height;
         if !config.draw_below
-            && ((res.time - FADEOUT_TIME >= self.time) || (self.fake && res.time >= self.time) || (self.time > res.time && base <= -0.0075))
+            && ((res.time - FADEOUT_TIME >= self.time) || (self.fake && res.time >= self.time) || (self.time > res.time && base <= -0.001))
             && !matches!(self.kind, NoteKind::Hold { .. })
         {
             return;
