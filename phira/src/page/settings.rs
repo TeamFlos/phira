2162e7ecac6e9c5f416435d9e65f268515a3d505
ChickenPige0n <2336983354@qq.com>
2025-05-08 19:04:07 +0800

    feat: add option to disable effect

--- phira/src/page/settings.rs
+++ phira/src/page/settings.rs
@@ -617,6 +617,7 @@ struct ChartList {
     show_acc_btn: DRectButton,
     dc_pause_btn: DRectButton,
     dhint_btn: DRectButton,
+    disable_effect_btn: DRectButton,
     opt_btn: DRectButton,
     speed_slider: Slider,
     size_slider: Slider,
@@ -628,6 +629,7 @@ impl ChartList {
             show_acc_btn: DRectButton::new(),
             dc_pause_btn: DRectButton::new(),
             dhint_btn: DRectButton::new(),
+            disable_effect_btn: DRectButton::new(),
             opt_btn: DRectButton::new(),
             speed_slider: Slider::new(0.5..2., 0.05),
             size_slider: Slider::new(0.8..1.2, 0.005),
@@ -651,6 +653,10 @@ impl ChartList {
         }
         if self.dhint_btn.touch(touch, t) {
             config.double_hint ^= true;
+            return Ok(Some(true));
+        }
+        if self.disable_effect_btn.touch(touch, t) {
+            config.disable_effect ^= true;
             return Ok(Some(true));
         }
         if self.opt_btn.touch(touch, t) {
@@ -695,6 +701,10 @@ impl ChartList {
         item! {
             render_title(ui, tl!("item-dhint"), Some(tl!("item-dhint-sub")));
             render_switch(ui, rr, t, &mut self.dhint_btn, config.double_hint);
+        }
+        item! {
+            render_title(ui, tl!("item-disable-effect"), Some(tl!("item-disable-effect-sub")));
+            render_switch(ui, rr, t, &mut self.disable_effect_btn, config.disable_effect);
         }
         item! {
             render_title(ui, tl!("item-opt"), Some(tl!("item-opt-sub")));
