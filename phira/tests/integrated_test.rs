502a2d8ebdb58bb625291a659a09ae665f1250bb
sjfhsjfh <sjfhsjfh@qq.com>
2026-03-09 01:00:18 +0800

    test: better l10n tests

--- phira/tests/integrated_test.rs
+++ phira/tests/integrated_test.rs
@@ -2,8 +2,5 @@ use prpr_l10n::tools::check_langfile;
 
 #[test]
 fn check_all() {
-    match check_langfile(concat!(env!("CARGO_MANIFEST_DIR"), "/locales/")) {
-        Ok(_) => {}
-        Err(e) => panic!("Error: {}", e),
-    }
+    check_langfile(concat!(env!("CARGO_MANIFEST_DIR"), "/locales/")).expect("l10n check failed");
 }
