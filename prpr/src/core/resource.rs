b35c0e329b2f16e7b9c08ff35f20097c822721f4
ljlvink <qpalzmlong@foxmail.com>
2026-03-17 00:15:07 +0800

    fix: clippy

--- prpr/src/core/resource.rs
+++ prpr/src/core/resource.rs
@@ -27,15 +27,15 @@ pub const MAX_SIZE: usize = 64; // needs tweaking
 pub static DPI_VALUE: AtomicU32 = AtomicU32::new(250);
 pub const BUFFER_SIZE: usize = 1024;
 
+//code from https://github.com/macnelly/quad-svg/blob/master/src/lib.rs
 fn svg_to_png(svg_str: &str) -> Vec<u8> {
     let opt = resvg::usvg::Options::default();
     let mut tree = resvg::usvg::Tree::from_str(svg_str, &opt).unwrap();
     let mut fontdb = fontdb::Database::new();
     fontdb.load_system_fonts();
-    tree.convert_text(&fontdb);
+    tree.convert_text(&fontdb, opt.keep_named_groups);
     let pixmap_size = tree.size.to_screen_size();
     let mut pixmap = resvg::tiny_skia::Pixmap::new(pixmap_size.width(), pixmap_size.height()).unwrap();
-
     resvg::render(&tree, resvg::usvg::FitTo::Original, resvg::tiny_skia::Transform::default(), pixmap.as_mut()).unwrap();
     pixmap.encode_png().unwrap()
 }
