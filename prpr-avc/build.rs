2da21e533a3af4f400c0f056fc2bf7ced3c90766
mivik <mivikq@gmail.com>
2026-08-04 22:52:07 +0800

    refactor: simplify prpr-avc build script

--- prpr-avc/build.rs
+++ prpr-avc/build.rs
@@ -1,15 +1,11 @@
 use std::{
-    env, fs,
-    fs::OpenOptions,
-    io,
+    env, fs, io,
     path::{Component, Path, PathBuf},
-    process, thread,
-    time::{Duration, Instant, SystemTime, UNIX_EPOCH},
+    time::Duration,
 };
 
 const BASE_URL: &str = "https://github.com/TeamFlos/prpr-avc-ffmpeg/releases/download";
 const EXPECTED_LIBS: &[&str] = &["libavcodec.a", "libavformat.a", "libavutil.a", "libswresample.a", "libswscale.a"];
-const LOCK_TIMEOUT: Duration = Duration::from_secs(120);
 
 fn main() {
     if let Err(err) = run() {
@@ -48,26 +44,15 @@ fn ensure_static_lib(libs_dir: &Path, target: &str, version: &str) -> Result<(),
         return Ok(());
     }
 
-    fs::create_dir_all(libs_dir)?;
-    let lock_path = libs_dir.join(format!(".{target}.lock"));
-    let _lock = CacheLock::acquire(lock_path)?;
+    fs::create_dir_all(&target_dir)?;
 
-    if cache_is_valid(&target_dir, version) {
-        return Ok(());
-    }
-
-    let temp_dir = unique_path(libs_dir, target, "download");
-    fs::create_dir(&temp_dir)?;
-    let mut temp_guard = RemoveOnDrop::new(temp_dir.clone());
     let url = format!("{BASE_URL}/{version}/{target}.tar.gz");
     println!("cargo:warning=[prpr-avc] Downloading FFmpeg static libraries for {target} ({version})");
 
-    download_and_extract(&url, &temp_dir)?;
-    validate_libs(&temp_dir)?;
-    fs::write(temp_dir.join(".version"), format!("{version}\n"))?;
+    download_and_extract(&url, &target_dir)?;
+    validate_libs(&target_dir)?;
+    fs::write(target_dir.join(".version"), format!("{version}\n"))?;
 
-    replace_cache(&temp_dir, &target_dir)?;
-    temp_guard.disarm();
     Ok(())
 }
 
@@ -117,84 +102,4 @@ fn download_and_extract(url: &str, output_dir: &Path) -> Result<(), Box<dyn std:
 
 fn safe_archive_path(path: &Path) -> bool {
     !path.as_os_str().is_empty() && path.components().all(|component| matches!(component, Component::Normal(_)))
-}
-
-fn replace_cache(source: &Path, destination: &Path) -> io::Result<()> {
-    if !destination.exists() {
-        return fs::rename(source, destination);
-    }
-
-    let parent = destination
-        .parent()
-        .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidInput, "cache path has no parent"))?;
-    let name = destination
-        .file_name()
-        .and_then(|name| name.to_str())
-        .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidInput, "invalid cache path"))?;
-    let backup = unique_path(parent, name, "old");
-    fs::rename(destination, &backup)?;
-
-    if let Err(err) = fs::rename(source, destination) {
-        let _ = fs::rename(&backup, destination);
-        return Err(err);
-    }
-    fs::remove_dir_all(backup)
-}
-
-fn unique_path(parent: &Path, target: &str, label: &str) -> PathBuf {
-    let nanos = SystemTime::now().duration_since(UNIX_EPOCH).unwrap_or_default().as_nanos();
-    parent.join(format!(".{target}.{label}.{}.{nanos}", process::id()))
-}
-
-struct CacheLock {
-    path: PathBuf,
-}
-
-impl CacheLock {
-    fn acquire(path: PathBuf) -> io::Result<Self> {
-        let started = Instant::now();
-        loop {
-            match OpenOptions::new().write(true).create_new(true).open(&path) {
-                Ok(_) => return Ok(Self { path }),
-                Err(err) if err.kind() == io::ErrorKind::AlreadyExists => {
-                    if started.elapsed() >= LOCK_TIMEOUT {
-                        return Err(io::Error::new(
-                            io::ErrorKind::TimedOut,
-                            format!("timed out waiting for cache lock {}; remove it if no other build is running", path.display()),
-                        ));
-                    }
-                    thread::sleep(Duration::from_millis(100));
-                }
-                Err(err) => return Err(err),
-            }
-        }
-    }
-}
-
-impl Drop for CacheLock {
-    fn drop(&mut self) {
-        let _ = fs::remove_file(&self.path);
-    }
-}
-
-struct RemoveOnDrop {
-    path: Option<PathBuf>,
-}
-
-impl RemoveOnDrop {
-    fn new(path: PathBuf) -> Self {
-        Self { path: Some(path) }
-    }
-
-    fn disarm(&mut self) {
-        self.path = None;
-    }
-}
-
-impl Drop for RemoveOnDrop {
-    fn drop(&mut self) {
-        if let Some(path) = &self.path {
-            let _ = fs::remove_dir_all(path);
-        }
-    }
 }
